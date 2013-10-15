e66acd8e623fa95c9ff34ddc9b872963a977ff74
Brian Szmyd <brian.szmyd@gmail.com>
2013-10-15 09:57:54 -0600

    acu131338: Fix tarball creation of nested cookbooks.

--- lib/right_scraper/scanners/cookbook_metadata.rb
+++ lib/right_scraper/scanners/cookbook_metadata.rb
@@ -163,8 +163,8 @@ module RightScraper::Scanners
           # arrest
           knife_metadata_script_path = ::File.join(tmpdir, KNIFE_METADATA_SCRIPT_NAME)
           cookbook_tarball_path = ::File.join(tmpdir, TARBALL_ARCHIVE_NAME)
-          jailed_cookbook_dir = ::File.join(tmpdir, UNDEFINED_COOKBOOK_NAME)
-          jailed_cookbook_dir = ::File.join(jailed_cookbook_dir, @cookbook.pos) unless @cookbook.pos == '.'
+          jailed_repo_dir = ::File.join(tmpdir, UNDEFINED_COOKBOOK_NAME)
+          jailed_cookbook_dir = (@cookbook.pos == '.' && jailed_repo_dir) || ::File.join(jailed_repo_dir, @cookbook.pos)
           jailed_metadata_json_path = ::File.join(jailed_cookbook_dir, JSON_METADATA)
           freed_metadata_json_path = ::File.join(tmpdir, JSON_METADATA)
 
@@ -175,13 +175,13 @@ module RightScraper::Scanners
           copy_out = { jailed_metadata_json_path => freed_metadata_json_path }
 
           # prosecute
-          create_cookbook_tarball(cookbook_tarball_path, copy_in, jailed_cookbook_dir)
+          create_cookbook_tarball(cookbook_tarball_path, copy_in, jailed_repo_dir)
 
           # jail
           warden = create_warden
           begin
             # Deploy our cookbook
-            cmd = "tar -Pxf #{cookbook_tarball_path}"
+            cmd = "tar -Pxf #{cookbook_tarball_path.inspect}"
             warden.run_command_in_jail(cmd, cookbook_tarball_path, nil)
 
             # Generate the metadata
