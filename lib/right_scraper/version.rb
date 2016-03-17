261720f1c5c2bebd6486a403315bf0041500b2cb
Scott Messier <scott@rightscale.com>
2016-03-17 11:03:05 -0700

    changed to take secrets from environment instead of configuration files, which is safer when executing docker containers v5.2.2

--- lib/right_scraper/version.rb
+++ lib/right_scraper/version.rb
@@ -23,7 +23,7 @@
 
 module RightScraper
   # for gemspec, etc.
-  GEM_VERSION = '5.2.1'
+  GEM_VERSION = '5.2.2'
 
   # (Fixnum) protocol versioning scheme; prepended to hashes to
   # prevent collisions.
