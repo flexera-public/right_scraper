13729b350725dc02225c9bccfa7bd86958099167
Scott Messier <scott@rightscale.com>
2016-03-28 14:24:49 -0700

    change to write errors in JSON to stdout to distinguish from docker client errors v5.2.7

--- lib/right_scraper/version.rb
+++ lib/right_scraper/version.rb
@@ -23,7 +23,7 @@
 
 module RightScraper
   # for gemspec, etc.
-  GEM_VERSION = '5.2.6'
+  GEM_VERSION = '5.2.7'
 
   # (Fixnum) protocol versioning scheme; prepended to hashes to
   # prevent collisions.
