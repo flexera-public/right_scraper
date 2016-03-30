a1af1e05a4dec2b856b5f36ecd96ca4fe8899c0b
Scott Messier <scott@rightscale.com>
2016-03-30 15:33:57 -0700

    also ignore SIGHUP while scraping

--- lib/right_scraper/version.rb
+++ lib/right_scraper/version.rb
@@ -23,7 +23,7 @@
 
 module RightScraper
   # for gemspec, etc.
-  GEM_VERSION = '5.2.7'
+  GEM_VERSION = '5.2.8'
 
   # (Fixnum) protocol versioning scheme; prepended to hashes to
   # prevent collisions.
